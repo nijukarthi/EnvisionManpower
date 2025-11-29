e530fa034f7c568bbf93dfbae7fd2e1e5734f687
Pavithradevi <paviguna116@gmail.com>
2025-11-29 10:59:12 +0530

    api, fix: integrated PO form api and PO list api and resolved the UI issue

--- src/app/pages/po-assign-form/po-assign-form.ts
+++ src/app/pages/po-assign-form/po-assign-form.ts
@@ -1,7 +1,8 @@
 import { Apiservice } from '@/service/apiservice/apiservice';
 import { Shared } from '@/service/shared';
 import { Component, inject, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 import { MessageService } from 'primeng/api';
 
 @Component({
@@ -15,6 +16,7 @@ export class PoAssignForm implements OnInit {
 
   selectedSpnId = 0;
   selectedExistingSpnId = 0;
+  duration = 0;
 
   consultancyList: any;
   spnInfoList: any;
@@ -23,8 +25,13 @@ export class PoAssignForm implements OnInit {
   };
   demandList: any;
   selectedExistingSpn: any;
+  newEmployeeList: any;
+  existingEmployeeList: any;
 
-  constructor(private apiService: Apiservice, private messageService: MessageService){}
+  newEmployeeLineItems: any[] = [];
+  existingEmployeeLineItems: any[] = [];
+
+  constructor(private apiService: Apiservice, private messageService: MessageService, private router: Router){}
 
   mapPOForm = this.fb.group({
     poNumber: [0],
@@ -35,69 +42,42 @@ export class PoAssignForm implements OnInit {
     validFrom: [''],
     validTo: [''],
     totalValue: [0],
-    items: this.fb.array([this.addEmployee()])
+    newEmployeeItems: this.fb.array([]),
+    existingEmployeeItems: this.fb.array([])
   })
 
-  addEmployee(){
+  get newEmployeeItems(){
+    return this.mapPOForm.get('newEmployeeItems') as FormArray;
+  }
+
+  get existingEmployeeItems(){
+    return this.mapPOForm.get('existingEmployeeItems') as FormArray;
+  }
+
+  addLineItems(emp?: any){
     return this.fb.group({
       candidate: this.fb.group({
-        candidateId: [0]
+        candidateId: [emp?.candidateId]
       }),
       monthsAllowed: [0],
       unitRate: [0],
-      taxRate: [0]
+      taxRate: [0],
+      taxAmount: [0],
+      itemTotal: [0]
     })
   }
-
-  newEmployeeList = [
-    {
-      id: 1,
-      employeeCode: 'EC-0005',
-      employeeName: 'Praveen',
-      duration: 5,
-      unitPrice: 1000,
-      taxRate: 4000,
-      amount: 10000,
-      totalValue: 50000
-    },
-    {
-      id: 2,
-      employeeCode: 'EC-0006',
-      employeeName: 'Kayal',
-      duration: 6,
-      unitPrice: 2000,
-      taxRate: 4000,
-      amount: 15000,
-      totalValue: 550000
-    }
-  ]
-
-    existingEmployeeList = [
-    {
-      id: 1,
-      employeeCode: 'EC-0007',
-      employeeName: 'Kumar',
-      duration: 5,
-      unitPrice: 1000,
-      taxRate: 4000,
-      amount: 10000,
-      totalValue: 50000
-    },
-    {
-      id: 2,
-      employeeCode: 'EC-0008',
-      employeeName: 'Sandhya',
-      duration: 6,
-      unitPrice: 2000,
-      taxRate: 4000,
-      amount: 15000,
-      totalValue: 550000
-    }
-  ]
 
   ngOnInit(): void {
     this.fetchConsultancyList();
     this.fetchSpnInfoList();
+
+    const form = this.mapPOForm;
+
+    form.get('validFrom')?.valueChanges.subscribe(() => this.updateDuration());
+    form.get('validTo')?.valueChanges.subscribe(() => this.updateDuration());
+
+    this.addNewEmployeeLineItems();
+    this.addExistingEmployeeLineItems();
   }
 
   fetchConsultancyList(){
@@ -114,6 +94,52 @@ export class PoAssignForm implements OnInit {
     } catch (error) {
       console.log(error);
     }
+  }
+
+  updateDuration(){
+    const validFrom = this.mapPOForm.get('validFrom')?.value;
+    const validTo = this.mapPOForm.get('validTo')?.value;
+
+    if(validFrom && validTo){
+      const start = new Date(validFrom);
+      const end = new Date(validTo);
+
+      const years = end.getFullYear() - start.getFullYear();
+      const months = end.getMonth() - start.getMonth();
+
+      this.duration = years * 12 + months;
+    } else {
+      this.duration = 0;
+    }
+  }
+
+  addNewEmployeeLineItems(){
+    const newItem = this.newEmployeeLineItems[0];
+    this.newEmployeeLineItems.push(newItem);
+    // this.newEmployeeList = [];
+    // this.selectedSpnId = 0;
+    // this.selectedSpn = [];
+  } 
+
+  addExistingEmployeeLineItems(){
+    const newItem = this.existingEmployeeLineItems[0];
+    this.existingEmployeeLineItems.push(newItem);
+  }
+
+  subscribeToItemChanges(item: FormGroup){
+    item.valueChanges.subscribe(val => {
+      const months = val.monthsAllowed;
+      const unitPrice = val.unitRate;
+      const taxRate = val.taxRate;
+      console.log(months, unitPrice, taxRate);
+
+      const taxAmount = (unitPrice * (taxRate / 100) + 100);
+      const itemTotal = months * taxAmount;
+      item.patchValue({
+        taxAmount: taxAmount,
+        itemTotal: itemTotal
+      }, { emitEvent: false })
+    })
   }
 
   fetchSpnInfoList(){
@@ -170,6 +196,18 @@ export class PoAssignForm implements OnInit {
     }
   }
 
+  populateNewEmployeeList(){
+    this.newEmployeeItems.clear();
+
+    this.newEmployeeList.forEach((emp: any) => {
+      this.newEmployeeItems.push(this.addLineItems(emp));
+    });
+
+    this.newEmployeeItems.controls.forEach((item, index) => {
+      this.subscribeToItemChanges(item as FormGroup)
+    });
+  }
+
   selectedDemand(demandId: number){
     try {
       const data = {
@@ -180,6 +218,14 @@ export class PoAssignForm implements OnInit {
       this.apiService.fetchDemandCandidates(data).subscribe({
         next: val => {
           console.log(val);
+          const newEmployeeItems = val?.data;
+
+          const selectedConsultancy = this.mapPOForm.get('consultancy.userId')?.value;
+
+          this.newEmployeeList = newEmployeeItems.filter((e: any) => e.consultancyId === selectedConsultancy);
+          this.populateNewEmployeeList();
+
+          console.log(this.newEmployeeList);
         },
         error: err => {
           console.log(err);
@@ -194,6 +240,18 @@ export class PoAssignForm implements OnInit {
     }
   }
 
+  populateExistingEmployeeList(){
+    this.existingEmployeeItems.clear();
+
+    this.existingEmployeeList.forEach((emp: any) => {
+      this.existingEmployeeItems.push(this.addLineItems(emp));
+    });
+
+    this.existingEmployeeItems.controls.forEach((item, index) => {
+      this.subscribeToItemChanges(item as FormGroup)
+    });
+  }
+
   fetchSpnCandidates(){
     try {
       const data = {
@@ -205,9 +263,21 @@ export class PoAssignForm implements OnInit {
       this.apiService.fetchSpnCandidates(data).subscribe({
         next: val => {
           console.log(val);
+          const existingEmployeeItems = val?.data;
+
+          const selectedConsultancy = this.mapPOForm.get('consultancy.userId')?.value;
+
+          this.existingEmployeeList = existingEmployeeItems.filter((e: any) => e.consultancyId === selectedConsultancy);
+          this.populateExistingEmployeeList();
+          console.log(this.existingEmployeeList);
         },
         error: err => {
           console.log(err);
+
+          if (err.status === 400) {
+            this.messageService.add({severity: 'error', summary: 'Error', detail: err.error.detail });
+            this.existingEmployeeList = [];
+          }
         }
       })
     } catch (error) {
@@ -215,7 +285,63 @@ export class PoAssignForm implements OnInit {
     }
   }
 
+  removeNewEmployee(index: number){
+    this.newEmployeeItems.removeAt(index);
+  }
+
+  removeExistingEmployee(index: number){
+    console.log(index);
+    this.existingEmployeeItems.removeAt(index);
+  }
+
+  mapItems(list: any[]){
+    return list.map((item: any) => ({
+      candidate: {
+        candidateId: item.candidate.candidateId
+      },
+      monthsAllowed: item.monthsAllowed,
+      unitRate: item.unitRate,
+      taxRate: item.taxRate
+    }))
+  }
+
   onSubmit(){
-    console.log(this.mapPOForm.value);
+    try {     
+      console.log(this.mapPOForm.value);
+
+      const data = {
+        poNumber: this.mapPOForm.get('poNumber')?.value,
+        consultancy: {
+          userId: this.mapPOForm.get('consultancy.userId')?.value
+        },
+        poDate: this.mapPOForm.get('poDate')?.value,
+        validFrom: this.mapPOForm.get('validFrom')?.value,
+        validTo: this.mapPOForm.get('validTo')?.value,
+        totalValue: this.mapPOForm.get('totalValue')?.value,
+        items: [
+          ...this.mapItems(this.newEmployeeItems.value),
+          ...this.mapItems(this.existingEmployeeItems.value)
+        ]
+      }
+
+      console.log(data);
+
+      this.apiService.mapNewPurchaseOrder(data).subscribe({
+        next: val => {
+          console.log(val);
+          this.messageService.add({severity:'success', summary: 'Success', detail: 'Purchase Order Created Successfully'});
+          this.router.navigate(['/home/po-assign']);
+        },
+        error: err => {
+          console.log(err);
+          
+          if (err.status == 400) {
+            this.messageService.add({severity: 'error', summary: 'Error', detail: err.error.detail});
+          }
+        }
+      })
+    } catch (error) {
+      console.log(error);
+    }
   }
 }
