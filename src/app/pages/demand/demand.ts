a44564f35f46e81da2273edc91b29547065a9150
boomika <boomikam85@gmail.com>
2025-12-11 18:06:39 +0530

    fix:Validation in ResourcePool-Candidate Form and Manpower Request Form

--- src/app/pages/demand/demand.ts
+++ src/app/pages/demand/demand.ts
@@ -6,218 +6,230 @@ import { UserGroups } from '@/models/usergroups/usergroups.enum';
 import { MessageService } from 'primeng/api';
 
 @Component({
-  selector: 'app-demand',
-  imports: [Shared],
-  templateUrl: './demand.html',
-  styleUrl: './demand.scss'
+    selector: 'app-demand',
+    imports: [Shared],
+    templateUrl: './demand.html',
+    styleUrl: './demand.scss'
 })
 export class Demand implements OnInit {
+    selectedPCodes: any = '';
+    PCodeList: any;
+    categoryList: any;
+    spnInfoList: any;
+    projectDetails: any;
 
-  selectedPCodes:any = "";
-  PCodeList: any;
-  categoryList: any;
-  spnInfoList: any;
-  projectDetails: any;
+    minDate: Date | undefined;
+    releaseMinDate: Date | undefined;
 
-  minDate: Date | undefined;
-  releaseMinDate: Date | undefined;
+    loading = false;
 
-  loading = false;
+    private fb = inject(FormBuilder);
 
-  private fb = inject(FormBuilder);
+    userGroups = UserGroups;
 
-  userGroups = UserGroups;
+    requestedBy = sessionStorage.getItem('userName');
 
-  requestedBy = sessionStorage.getItem('userName');
+    selectedState: any = '';
+    plannedDeploymentDate: any;
 
-  selectedState:any = "";
+    constructor(
+        private apiService: Apiservice,
+        private messageService: MessageService
+    ) {}
 
-  constructor(private apiService: Apiservice, private messageService: MessageService) {}
-
-  demandForm = this.fb.group({
-    project: this.fb.group({
-      projectId: [null, Validators.required],
-    }),
-    category: this.fb.group({
-      categoryId: [0]
-    }),
-    demandDetails: this.fb.array([this.createDemandDetails()]),
-  });
-
-  createDemandDetails() {
-    return this.fb.group({
-      spn: this.fb.group({
-        spnId: ['', Validators.required]
-      }),
-      spnDescription: ['', Validators.required],
-      experience: [{ value: '', disabled: true }, Validators.required],
-      quantity: [null, [Validators.required, Validators.min(1)]],
-      plannedDeploymentDate: ['', Validators.required],
-      plannedReleaseDate: ['', Validators.required]
+    demandForm = this.fb.group({
+        project: this.fb.group({
+            projectId: [null, Validators.required]
+        }),
+        category: this.fb.group({
+            categoryId: [0]
+        }),
+        demandDetails: this.fb.array([this.createDemandDetails()])
     });
-  }
 
-  ngOnInit(): void {
-    this.fetchPCodes();
-    this.fetchActiveCategory();
-    this.fetchSpnInfo();
-
-    const today = new Date();
-    this.minDate = new Date(today.setDate(today.getDate() + 30));
-  }
-
-  get demandDetails(): FormArray {
-    return this.demandForm.get('demandDetails') as FormArray;
-  }
-
-  selectedPCode(projectId: number){
-    try {   
-      console.log(projectId);
-  
-      const data = {
-        projectId: projectId
-      }
-  
-      this.apiService.viewProject(data).subscribe({
-        next: val => {
-          console.log(val);
-          this.projectDetails = val.data;     
-        },
-        error: err => {
-          console.log(err);
-        }
-      })
-    } catch (error) {
-      console.log(error);
+    createDemandDetails() {
+        return this.fb.group({
+            spn: this.fb.group({
+                spnId: ['', Validators.required]
+            }),
+            spnDescription: ['', Validators.required],
+            experience: [{ value: '', disabled: true }, Validators.required],
+            quantity: [null, [Validators.required, Validators.minLength(1), Validators.maxLength(100)]],
+            plannedDeploymentDate: ['', Validators.required],
+            plannedReleaseDate: ['', Validators.required]
+        });
     }
-  }
 
-  fetchPCodes(){
-    try {   
-      this.apiService.fetchProjectCodes('').subscribe({
-        next: val => {
-          console.log(val);
-          this.PCodeList = val.data;
-        },
-        error: err => {
-          console.log(err);
-        }
-      })
-    } catch (error) {
-      console.log(error);
+    ngOnInit(): void {
+        this.fetchPCodes();
+        this.fetchActiveCategory();
+        this.fetchSpnInfo();
+
+        const today = new Date();
+        this.minDate = new Date(today.setDate(today.getDate() + 30));
     }
-  }
 
-
-  fetchActiveCategory(){
-    try { 
-      this.apiService.fetchActiveCategory('').subscribe({
-        next: val => {
-          console.log(val);
-          this.categoryList = val.data;
-        },
-        error: err => {
-          console.log(err);
-        }
-      })
-    } catch (error) {
-      console.log(error);
+    get demandDetails(): FormArray {
+        return this.demandForm.get('demandDetails') as FormArray;
     }
-  }
-
-  fetchSpnInfo(){
-    try {
-      this.apiService.fetchSpnInfo('').subscribe({
-        next: val => {
-          console.log("Spn Info", val);
-          this.spnInfoList = val.data;
-        },
-        error: err => {
-          console.log(err);
-        }
-      })
-    } catch (error) {
-      console.log(error);
+    getQuantityControl(index: number) {
+        return this.demandDetails.at(index).get('quantity');
     }
-  }
 
-  addSpnRow(): void {
-    const spnGroup = this.fb.group({
-      spn: this.fb.group({
-        spnId: ['', Validators.required]
-      }),
-      spnDescription: ['', Validators.required],
-      experience: [{ value: '', disabled: true }, Validators.required],
-      quantity: [null, [Validators.required, Validators.min(1)]],
-      plannedReleaseDate: ['', Validators.required],
-      plannedDeploymentDate: ['', Validators.required],
-    });
-    this.demandDetails.push(spnGroup);
-  }
+    getDeploymentDate(index: number) {
+        return this.demandDetails.at(index).get('plannedDeploymentDate');
+    }
+    getReleaseDate(index: number) {
+        return this.demandDetails.at(index).get('plannedReleaseDate');
+    }
 
-  deleteSpnRow(index: number): void {
-    this.demandDetails.removeAt(index);
-  }
+    selectedPCode(projectId: number) {
+        try {
+            console.log(projectId);
 
-  selectedSpnCode(event: any, rowIndex: number){
-    console.log(event);
+            const data = {
+                projectId: projectId
+            };
 
-    this.demandDetails.at(rowIndex).patchValue({
-      spnDescription: event,
-      experience: event
-    })
-  }
-
-  selectedSpnDescription(event: any, rowIndex: number){
-    console.log(event);
-
-    this.demandDetails.at(rowIndex).patchValue({
-      spn: {
-        spnId: event
-      },
-      experience: event
-    })
-  }
-
-  selectedDeploymentDate(deploymentDate: any){
-    console.log(deploymentDate);
-
-    const releaseStart = new Date(deploymentDate);
-    releaseStart.setDate(releaseStart.getDate() + 7);
-    this.releaseMinDate = releaseStart;
-  }
-
-  onSubmit(): void {
-    console.log(this.demandForm.value);
-
-    const data = {
-      project: {
-        projectId: this.demandForm.get('project.projectId')?.value
-      },
-      category: {
-        categoryId: this.demandForm.get('category.categoryId')?.value
-      },
-      demandDetails: this.demandForm.get('demandDetails')?.value
-    };
-
-    console.log(data);
-
-    this.apiService.createRequesition(data).subscribe({
-      next: val => {
-        console.log(val);
-        this.messageService.add({severity: 'success', summary: 'Success', detail: 'Manpower Request Created Successfully'});
-        this.demandForm.reset();
-        this.demandDetails.clear();
-        this.projectDetails = null;
-        this.addSpnRow();
-      },
-      error: err => {
-        console.log(err);
-
-        if (err.status === 400) {
-          this.messageService.add({severity: 'error', summary: 'Error', detail: err.error.detail});
+            this.apiService.viewProject(data).subscribe({
+                next: (val) => {
+                    console.log(val);
+                    this.projectDetails = val.data;
+                },
+                error: (err) => {
+                    console.log(err);
+                }
+            });
+        } catch (error) {
+            console.log(error);
         }
-      }
-    })
-  }
+    }
+
+    fetchPCodes() {
+        try {
+            this.apiService.fetchProjectCodes('').subscribe({
+                next: (val) => {
+                    console.log(val);
+                    this.PCodeList = val.data;
+                },
+                error: (err) => {
+                    console.log(err);
+                }
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    fetchActiveCategory() {
+        try {
+            this.apiService.fetchActiveCategory('').subscribe({
+                next: (val) => {
+                    console.log(val);
+                    this.categoryList = val.data;
+                },
+                error: (err) => {
+                    console.log(err);
+                }
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    fetchSpnInfo() {
+        try {
+            this.apiService.fetchSpnInfo('').subscribe({
+                next: (val) => {
+                    console.log('Spn Info', val);
+                    this.spnInfoList = val.data;
+                },
+                error: (err) => {
+                    console.log(err);
+                }
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    addSpnRow(): void {
+        const spnGroup = this.fb.group({
+            spn: this.fb.group({
+                spnId: ['', Validators.required]
+            }),
+            spnDescription: ['', Validators.required],
+            experience: [{ value: '', disabled: true }, Validators.required],
+            quantity: [null, [Validators.required, Validators.min(1)]],
+            plannedReleaseDate: ['', Validators.required],
+            plannedDeploymentDate: ['', Validators.required]
+        });
+        this.demandDetails.push(spnGroup);
+    }
+
+    deleteSpnRow(index: number): void {
+        this.demandDetails.removeAt(index);
+    }
+
+    selectedSpnCode(event: any, rowIndex: number) {
+        console.log(event);
+
+        this.demandDetails.at(rowIndex).patchValue({
+            spnDescription: event,
+            experience: event
+        });
+    }
+
+    selectedSpnDescription(event: any, rowIndex: number) {
+        console.log(event);
+
+        this.demandDetails.at(rowIndex).patchValue({
+            spn: {
+                spnId: event
+            },
+            experience: event
+        });
+    }
+
+    selectedDeploymentDate(deploymentDate: any) {
+        console.log(deploymentDate);
+
+        const releaseStart = new Date(deploymentDate);
+        releaseStart.setDate(releaseStart.getDate() + 7);
+        this.releaseMinDate = releaseStart;
+    }
+
+    onSubmit(): void {
+        console.log(this.demandForm.value);
+
+        const data = {
+            project: {
+                projectId: this.demandForm.get('project.projectId')?.value
+            },
+            category: {
+                categoryId: this.demandForm.get('category.categoryId')?.value
+            },
+            demandDetails: this.demandForm.get('demandDetails')?.value
+        };
+
+        console.log(data);
+
+        this.apiService.createRequesition(data).subscribe({
+            next: (val) => {
+                console.log(val);
+                this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Manpower Request Created Successfully' });
+                this.demandForm.reset();
+                this.demandDetails.clear();
+                this.projectDetails = null;
+                this.addSpnRow();
+            },
+            error: (err) => {
+                console.log(err);
+
+                if (err.status === 400) {
+                    this.messageService.add({ severity: 'error', summary: 'Error', detail: err.error.detail });
+                }
+            }
+        });
+    }
 }
--- src/app/pages/spn/spn-table/spn-table.ts
+++ src/app/pages/spn/spn-table/spn-table.ts
@@ -1,6 +1,6 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { TableModule } from "primeng/table";
-import { ButtonModule } from "primeng/button";
+import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
 import { Shared } from '@/service/shared';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ConfirmationService, MessageService } from 'primeng/api';
@@ -9,232 +9,227 @@ import { Apiservice } from '@/service/apiservice/apiservice';
 import { FormFieldError } from '@/directives/form-field-error';
 
 @Component({
-  selector: 'app-spn-table',
-  imports: [TableModule, ButtonModule, Shared, ReactiveFormsModule, FormFieldError],
-  templateUrl: './spn-table.html',
-  styleUrl: './spn-table.scss'
+    selector: 'app-spn-table',
+    imports: [TableModule, ButtonModule, Shared, ReactiveFormsModule, FormFieldError],
+    templateUrl: './spn-table.html',
+    styleUrl: './spn-table.scss'
 })
 export class SpnTable implements OnInit {
-  openSpn = false;
+    openSpn = false;
 
-  offSet = 0;
-  pageSize = 10;
-  first = 0;
-  totalRecords = 0;
+    offSet = 0;
+    pageSize = 10;
+    first = 0;
+    totalRecords = 0;
 
-  actionName = '';
+    actionName = '';
 
-  spnId: number | null = null;
+    spnId: number | null = null;
 
-  spnList: any;
+    spnList: any;
 
-  private fb = inject(FormBuilder);
+    private fb = inject(FormBuilder);
 
-  constructor(
-    private apiService: Apiservice,
-    private router: Router,
-    private messageService: MessageService,
-    private confirmationService: ConfirmationService
-  ){}
+    constructor(
+        private apiService: Apiservice,
+        private router: Router,
+        private messageService: MessageService,
+        private confirmationService: ConfirmationService
+    ) {}
 
-  spnForm = this.fb.group({
-    spnId: [0],
-    spnCode: ['', Validators.required],
-    spnDescription: ['', Validators.required],
-    experience: ['', Validators.required]
-  })
+    spnForm = this.fb.group({
+        spnId: [0],
+        spnCode: ['', Validators.required],
+        spnDescription: ['', Validators.required],
+        experience: ['', Validators.required]
+    });
 
-  ngOnInit(): void {
-    this.fetchActiveSpn();
-  }
-
-  experiences = [
-    { year: '0 year' },
-    { year: '3-5 years' },
-    { year: '5-8 years' },
-    { year: '6-8 years' },
-    { year: '8-10 years' },
-  ]
-  
-  get spnCode(){
-    return this.spnForm.get('spnCode');
-  }
-
-  get spnDescription(){
-    return this.spnForm.get('spnDescription');
-  }
-
-  get experience(){
-    return this.spnForm.get('experience');
-  }
-
-  getMenuItems(spn: any){
-    return [
-      {
-        label: 'Edit',
-        icon: 'pi pi-pencil',
-        command: () => this.editSpn(spn)
-      },
-      {
-        label: 'Delete',
-        icon: 'pi pi-trash',
-        command: () => this.deleteSpn(spn)
-      }
-    ]
-  }
-
-  addSpn(){
-    try {
-      this.openSpn = true;
-      this.actionName = 'Save';
-    } catch (error) {
-      console.log(error);
+    ngOnInit(): void {
+        this.fetchActiveSpn();
     }
-  }
 
-  fetchActiveSpn(){
-    try {
-      const data = {
-        offSet: this.offSet,
-        pageSize: this.pageSize
-      }
-      this.apiService.fetchActiveSpns(data).subscribe({
-        next: val => {
-          console.log(val);
-          this.spnList = val?.data.data;
-          this.totalRecords = val?.data.length ?? 0;
-        }
-      })
-    } catch (error) {
-      console.log(error);
+    experiences = [{ year: '0 year' }, { year: '3-5 years' }, { year: '5-8 years' }, { year: '6-8 years' }, { year: '8-10 years' }];
+
+    get spnCode() {
+        return this.spnForm.get('spnCode');
     }
-  }
 
-  editSpn(spn: any){
-    try {
-      this.spnId = spn.spnId;
-      this.openSpn = true;
-      this.actionName = 'Update';
-      this.spnForm.patchValue({
-        spnCode: spn.spnCode,
-        spnDescription: spn.spnDescription,
-        experience: spn.experience
-      })
-    } catch (error) {
-      console.log(error);
+    get spnDescription() {
+        return this.spnForm.get('spnDescription');
     }
-  }
 
-  onSubmit(){
-    try {
-      console.log(this.spnForm.value);
-      if (!this.spnId) {     
-        if (this.spnForm.valid) {
-          const payload = {
-            spnCode: this.spnForm.get('spnCode')?.value,
-            spnDescription: this.spnForm.get('spnDescription')?.value,
-            experience: this.spnForm.get('experience')?.value
-          }
+    get experience() {
+        return this.spnForm.get('experience');
+    }
 
-          this.apiService.createNewSpn(payload).subscribe({
-            next: val =>{
-              console.log(val);
-              this.messageService.add({severity: 'success', summary: 'Success', detail: 'SPN Created Successfully'});
-              this.openSpn = false;
-              this.spnForm.reset();
-              this.fetchActiveSpn();
-            }, error: err => {
-              console.log(err);
-
-              if (err.status === 400) {
-                this.messageService.add({severity: 'error', summary: 'Error', detail: err.error.detail})
-              }
-            }
-          })
-        }
-      } else {
-        if (this.spnForm.valid) {
-          const payload = {
-            spnId: this.spnId,
-            spnCode: this.spnForm.get('spnCode')?.value,
-            spnDescription: this.spnForm.get('spnDescription')?.value,
-            experience: this.spnForm.get('experience')?.value
-          }
-
-          this.apiService.updateSpn(payload).subscribe({
-            next: val => {
-              console.log(val);
-              this.messageService.add({severity: 'success', summary: 'Success', detail: 'SPN Updated Successfully'});
-              this.openSpn = false;
-              this.spnForm.reset();
-              this.fetchActiveSpn();
+    getMenuItems(spn: any) {
+        return [
+            {
+                label: 'Edit',
+                icon: 'pi pi-pencil',
+                command: () => this.editSpn(spn)
             },
-            error: err => {
-              console.log(err);
-
-              if (err.status === 400) {
-                this.messageService.add({severity: 'error', summary: 'Error', detail: err.error.detail});
-              }
+            {
+                label: 'Delete',
+                icon: 'pi pi-trash',
+                command: () => this.deleteSpn(spn)
             }
-          })
-        }
-      }
-    } catch (error) {
-      console.log(error);
-      this.messageService.add({severity: 'error', summary: 'Error', detail: 'Please Try Again'});
+        ];
     }
-  }
 
-  deleteSpn(spn: any){
-    this.confirmationService.confirm({
-      message: 'Do you want to delete this record?',
-      header: `Delete ${spn.spnCode}`,
-      icon: 'pi pi-info-circle',
-      rejectLabel: 'Cancel',
-      rejectButtonProps: {
-        label: 'Cancel',
-        severity: 'secondary',
-        outlined: true,
-      },
-      acceptButtonProps: {
-        label: 'Delete',
-        severity: 'danger'
-      },
-      accept: () => {
+    addSpn() {
         try {
-          const data = {
-            spnId: spn.spnId
-          }
-
-          this.apiService.deleteSpn(data).subscribe({
-            next: val => {
-              console.log(val);
-              this.messageService.add({severity: 'success', summary: 'Success', detail: 'SPN deleted Successfully'});
-              this.fetchActiveSpn();
-            },
-            error: err => {
-              console.log(err);
-              if (err.status === 400) {
-                this.messageService.add({severity: 'error', summary: 'Error', detail: `${err.error.detail}`});
-              }
-            }
-          })
+            this.openSpn = true;
+            this.actionName = 'Save';
         } catch (error) {
-          console.log(error);
+            console.log(error);
         }
-      }
-    })
-  }
-  
-  pageChange(event: any){
-    this.first = event.first;
-    this.offSet = event.first / event.rows;
-    this.pageSize = event.rows;
-    this.fetchActiveSpn();
-  }
+    }
 
-  onDialogClose(){
-    this.spnId = null;
-    this.spnForm.reset();
-  }
+    fetchActiveSpn() {
+        try {
+            const data = {
+                offSet: this.offSet,
+                pageSize: this.pageSize
+            };
+            this.apiService.fetchActiveSpns(data).subscribe({
+                next: (val) => {
+                    console.log(val);
+                    this.spnList = val?.data.data;
+                    this.totalRecords = val?.data.length ?? 0;
+                }
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    editSpn(spn: any) {
+        try {
+            this.spnId = spn.spnId;
+            this.openSpn = true;
+            this.actionName = 'Update';
+            this.spnForm.patchValue({
+                spnCode: spn.spnCode,
+                spnDescription: spn.spnDescription,
+                experience: spn.experience
+            });
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    onSubmit() {
+        try {
+            console.log(this.spnForm.value);
+            if (!this.spnId) {
+                if (this.spnForm.valid) {
+                    const payload = {
+                        spnCode: this.spnForm.get('spnCode')?.value,
+                        spnDescription: this.spnForm.get('spnDescription')?.value,
+                        experience: this.spnForm.get('experience')?.value
+                    };
+
+                    this.apiService.createNewSpn(payload).subscribe({
+                        next: (val) => {
+                            console.log(val);
+                            this.messageService.add({ severity: 'success', summary: 'Success', detail: 'SPN Created Successfully' });
+                            this.openSpn = false;
+                            this.spnForm.reset();
+                            this.fetchActiveSpn();
+                        },
+                        error: (err) => {
+                            console.log(err);
+
+                            if (err.status === 400) {
+                                this.messageService.add({ severity: 'error', summary: 'Error', detail: err.error.detail });
+                            }
+                        }
+                    });
+                }
+            } else {
+                if (this.spnForm.valid) {
+                    const payload = {
+                        spnId: this.spnId,
+                        spnCode: this.spnForm.get('spnCode')?.value,
+                        spnDescription: this.spnForm.get('spnDescription')?.value,
+                        experience: this.spnForm.get('experience')?.value
+                    };
+
+                    this.apiService.updateSpn(payload).subscribe({
+                        next: (val) => {
+                            console.log(val);
+                            this.messageService.add({ severity: 'success', summary: 'Success', detail: 'SPN Updated Successfully' });
+                            this.openSpn = false;
+                            this.spnForm.reset();
+                            this.fetchActiveSpn();
+                        },
+                        error: (err) => {
+                            console.log(err);
+
+                            if (err.status === 400) {
+                                this.messageService.add({ severity: 'error', summary: 'Error', detail: err.error.detail });
+                            }
+                        }
+                    });
+                }
+            }
+        } catch (error) {
+            console.log(error);
+            this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Please Try Again' });
+        }
+    }
+
+    deleteSpn(spn: any) {
+        this.confirmationService.confirm({
+            message: 'Do you want to delete this record?',
+            header: `Delete ${spn.spnCode}`,
+            icon: 'pi pi-info-circle',
+            rejectLabel: 'Cancel',
+            rejectButtonProps: {
+                label: 'Cancel',
+                severity: 'secondary',
+                outlined: true
+            },
+            acceptButtonProps: {
+                label: 'Delete',
+                severity: 'danger'
+            },
+            accept: () => {
+                try {
+                    const data = {
+                        spnId: spn.spnId
+                    };
+
+                    this.apiService.deleteSpn(data).subscribe({
+                        next: (val) => {
+                            console.log(val);
+                            this.messageService.add({ severity: 'success', summary: 'Success', detail: 'SPN deleted Successfully' });
+                            this.fetchActiveSpn();
+                        },
+                        error: (err) => {
+                            console.log(err);
+                            if (err.status === 400) {
+                                this.messageService.add({ severity: 'error', summary: 'Error', detail: `${err.error.detail}` });
+                            }
+                        }
+                    });
+                } catch (error) {
+                    console.log(error);
+                }
+            }
+        });
+    }
+
+    pageChange(event: any) {
+        this.first = event.first;
+        this.offSet = event.first / event.rows;
+        this.pageSize = event.rows;
+        this.fetchActiveSpn();
+    }
+
+    onDialogClose() {
+        this.spnId = null;
+        this.spnForm.reset();
+    }
 }
