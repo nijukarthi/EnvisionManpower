1ffdb56199526872e69f37c059068a8cdf37513f
boomika <boomikam85@gmail.com>
2025-12-17 17:57:23 +0530

    Api:Worked on filteration in invoice list

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    baseUrl: 'http://192.168.1.23:6060/api/' //'http://192.168.1.10:6060/api/'
+    baseUrl: 'http://192.168.1.21:6060/api/' //'http://192.168.1.10:6060/api/'
 };
 
 // 'https://d1fd0biuqahtd7.cloudfront.net/api/'
