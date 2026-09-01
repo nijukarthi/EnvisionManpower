11daff451194b69af3344817d6ae8274fd31874c
boomika <boomikam85@gmail.com>
2026-09-01 16:50:46 +0530

    Update development environment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,10 +1,6 @@
 export const environment = {
     production: false,
-<<<<<<< HEAD
     baseUrl: 'http://localhost:6060/api/' //'https://d1fd0biuqahtd7.cloudfront.net/api/' //''http://192.168.1.18:6060/api/' //'http://localhost:6060/api/' //'http://192.168.1.21:6060/api/' //'http://192.168.1.10:6060/api/'
-=======
-    baseUrl: 'https://d1fd0biuqahtd7.cloudfront.net/api/' //''http://192.168.1.18:6060/api/' //'https://d1fd0biuqahtd7.cloudfront.net/api/' //'http://192.168.1.21:6060/api/' //'http://192.168.1.10:6060/api/'
->>>>>>> c4cd81e3c858b7d0951116e3ba51167d63a78e38
 };
 
 // 'https://d1fd0biuqahtd7.cloudfront.net/api/'
